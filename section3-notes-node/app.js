c0e3bd6a15db28f6a5996e2f8051ce348d6bca87
Boris <boris9297@gmail.com>
2019-02-14 11:29:46 -0800

    3-11, npm init, import lodash

--- section3-notes-node/app.js
+++ section3-notes-node/app.js
@@ -4,11 +4,15 @@ console.log('starting app');
 const fs = require('fs');//import file system
 const os = require('os');//import OS
 const notes = require('./notes');
-
+const lodash = require('lodash');//lodash.com/docs/4.17.11
 // var user = os.userInfo()
 // fs.appendFileSync('greeting.txt', `hellow javascript, ${user.username}`);
 // console.log(user);
 
-console.log(notes.addNote);
-console.log(notes.addNote());
-console.log(notes.add(3, 99));
+// console.log(notes.addNote);
+// console.log(notes.addNote());
+// console.log(notes.add(3, 99));
+console.log(lodash.isString('hello'));
+console.log(lodash.isString(`hello`));
+console.log(lodash.uniq(['hi',`hi`]));
+console.log(lodash.uniq(['hi',`hi2`]));
