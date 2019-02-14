a9414ad210ee4e1bc917b10c43de253dfa7d5c28
Boris <boris9297@gmail.com>
2019-02-14 11:14:49 -0800

    require function from another file

--- section3-notes-node/app.js
+++ section3-notes-node/app.js
@@ -3,7 +3,12 @@ console.log('starting app');
 //https://nodejs.org/docs/latest/api/
 const fs = require('fs');//import file system
 const os = require('os');//import OS
+const notes = require('./notes');
 
-var user = os.userInfo()
-fs.appendFileSync('greeting.txt', `hellow javascript, ${user.username}`);
-console.log(user);
+// var user = os.userInfo()
+// fs.appendFileSync('greeting.txt', `hellow javascript, ${user.username}`);
+// console.log(user);
+
+console.log(notes.addNote);
+console.log(notes.addNote());
+console.log(notes.add(3, 99));
